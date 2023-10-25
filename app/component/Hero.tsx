a028dc64c2d873251fb82b36763af11c4e17f32f
Omer Mamaoro <endomamoro9@gmail.com>
2023-10-25 07:28:58 +0300

    text to white

--- app/component/Hero.tsx
+++ app/component/Hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-[30vh] w-full flex flex-col justify-center items-center text-2xl sm:text-4xl text-center">
+    <div className="h-[30vh] w-full flex flex-col justify-center items-center text-2xl sm:text-4xl text-white text-center">
       <div>
         <span>Seeing the weather of the whole world</span>
       </div>
-      <div className="mt-2 sm:mt-6">
+      <div className="mt-2 sm:mt-6 text-white">
         with{" "}
         <span className="text-4xl sm:text-5xl bg-gradient-to-r from-[#CAECFF] to-[#78CFFF] text-transparent bg-clip-text">
           Moro Weather
